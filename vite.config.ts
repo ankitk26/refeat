4a16f1da5f48a499134d321b11331adfa7919d4f
ankitk26 <myselfankit51@gmail.com>
2025-11-13 03:11:12 +0530

    try deploying using netlfiy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,11 @@ import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [
-    tsConfigPaths(),
-    tanstackStart(),
-    // react's vite plugin must come after start's vite plugin
-    viteReact(),
-  ],
+  plugins: [tsConfigPaths(), tanstackStart(), netlify(), viteReact()],
 });
