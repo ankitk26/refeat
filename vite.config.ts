e6bd1a94e045107c93cac5f263c1856b1eebb6b0
ankitk26 <myselfankit51@gmail.com>
2025-11-13 03:15:22 +0530

    add tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,17 @@ import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import netlify from "@netlify/vite-plugin-tanstack-start";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [tsConfigPaths(), tanstackStart(), netlify(), viteReact()],
+  plugins: [
+    tsConfigPaths(),
+    tanstackStart(),
+    netlify(),
+    viteReact(),
+    tailwindcss(),
+  ],
 });
